bac19e62f1d3cf5aba2ecdb04c8cff9e7f5d0321
baldingTomato <tomek.kluba12@gmail.com>
2024-04-25 16:16:37 +0200

    Finished tests for database.cpp

--- tests/database-ut.cpp
+++ tests/database-ut.cpp
@@ -69,3 +69,54 @@ TEST_F(DatabaseTest, DatabaseCanAddNewStudents) {
 
     EXPECT_EQ(output, "Znaleziono studenta:\n4 Gimli Syn Gloina Erebor 93081688751 Male\n");
 }
+
+TEST_F(DatabaseTest, DatabaseCanCheckPeselCorrectness) {
+    EXPECT_EQ(db.checkPeselCorrectness("72101496826"), true);
+    EXPECT_EQ(db.checkPeselCorrectness("72101496827"), false);
+    EXPECT_EQ(db.checkPeselCorrectness("80031313353"), true);
+    EXPECT_EQ(db.checkPeselCorrectness("8003a313353"), false);
+    EXPECT_EQ(db.checkPeselCorrectness("8003131%353"), false);
+    EXPECT_EQ(db.checkPeselCorrectness("8003131335"), false);
+    EXPECT_EQ(db.checkPeselCorrectness("80031313353234"), false);
+    EXPECT_EQ(db.checkPeselCorrectness(""), false);
+}
+
+TEST_F(DatabaseTest, DatabaseCanRemoveStudents) {
+    testing::internal::CaptureStdout();
+    db.removeStudent(1);
+
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "Student Kazimierz Wielki zostal usuniety z bazy!\n");
+}
+
+TEST_F(DatabaseTest, DatabasePrintsOutAllStudents) {
+    db.removeStudent(2);
+    db.removeStudent(3);
+    testing::internal::CaptureStdout();
+    db.selectWholeDatabase();
+
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "-------------------------------------------------------------------------------------------\n0 Scooter Alvarez 123 Tracewsky Street 55030101193 Male\n1 Kazimierz Wielki Wyszyńskiego 38 55030101230 Male\n-------------------------------------------------------------------------------------------\n");
+}
+
+TEST_F(DatabaseTest, DatabasePrintsOutAllStudentsSortedByLastNames) {
+    db.removeStudent(3);
+    testing::internal::CaptureStdout();
+    db.selectAndSortByLastName();
+
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "0 Scooter Alvarez 123 Tracewsky Street 55030101193 Male\n2 Son Gohan Góra Paozu 64050362718 Male\n1 Kazimierz Wielki Wyszyńskiego 38 55030101230 Male\n");
+}
+
+TEST_F(DatabaseTest, DatabasePrintsOutAllStudentsSortedByPesels) {
+    db.removeStudent(3);
+    testing::internal::CaptureStdout();
+    db.selectAndSortByPesel();
+
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_EQ(output, "0 Scooter Alvarez 123 Tracewsky Street 55030101193 Male\n1 Kazimierz Wielki Wyszyńskiego 38 55030101230 Male\n2 Son Gohan Góra Paozu 64050362718 Male\n");
+}
